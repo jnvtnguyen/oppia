3f0f3d153d5ebf0717582d763759917919f77c04
Justin Nguyen <jn-nguyen@outlook.com>
2025-07-20 18:18:15 -0700

    Updates

--- scripts/github_api.py
+++ scripts/github_api.py
@@ -24,7 +24,7 @@ import urllib.request
 
 from typing import Any, Dict, List, Optional, TypedDict
 
-REPOSITORY_OWNER = 'oppia'
+REPOSITORY_OWNER = 'jnvtnguyen'
 REPOSITORY_NAME = 'oppia'
 
 GITHUB_API_URL = 'https://api.github.com'
